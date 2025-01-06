eefe5f3a4b3f6ad14c970f8266427278806ed533
Mareks <robaldsmareks@gmail.com>
2025-01-06 21:42:13 +0200

    feat: added excel sheet name comparison and created names for the use in result excel

--- comparator/ExcelComparator.py
+++ comparator/ExcelComparator.py
@@ -1,28 +1,68 @@
 from models.ExcelFile import Excel
+from models.Sheets import Sheet
 from constants.emoji import EMOJIS
-from comparator.SheetComparator import compare_sheet_name
+from comparator.SheetComparator import compare_sheet_name, compare_row_count, compare_column_count
 import logging
 
 log = logging.getLogger()
 
 
-def get_sheet_count(excel1: Excel, excel2: Excel) -> int:
+def get_sheet_count(excel1: Excel, excel2: Excel) -> [int, bool]:
     sheetCount = 0
+    same_amount = False
     if len(excel1.sheets) == len(excel2.sheets):
         log.debug("Sheet count is the same")
         sheetCount = len(excel1.sheets)
+        same_amount = True
     else:
         log.debug("Sheet count is not the same, getting the lowest amount")
         sheetCount = min(len(excel1.sheets), len(excel2.sheets))
-    return sheetCount
+        same_amount = False
+    return sheetCount, same_amount
 
 
-def compare_sheet_names_lists(excel1: Excel, excel2: Excel, sheetCount: int) -> bool:
-    sheets1 = excel1.sheets.sort(key=str.lower())
-    sheets2 = excel2.sheets.sort(key=str.lower())
-    # TODO: Think of a better way
+def get_smaller_sheet_amount(sheets1: list[Sheet], sheets2: list[Sheet]) -> (list[Sheet], str):
 
-    for i in range(0, sheetCount):
-        if not compare_sheet_name(sheets1[i], sheets2[i]):
-            return False
-    return True
+    if len(sheets1) < len(sheets2):
+        return (sheets1, "Excel1"), (sheets2, "Excel2")
+
+    if len(sheets1) > len(sheets2):
+        return (sheets2, "Excel2"), (sheets1, "Excel1")
+
+
+def compare_sheet_names_lists(excel1: Excel, excel2: Excel) -> list[str]:
+    sheetCount, same_amount = get_sheet_count(excel1, excel2)
+    sheets1 = excel1.sheets
+    sheets2 = excel2.sheets
+    result = []
+    if same_amount:
+        for i in range(0, sheetCount):
+            if not compare_sheet_name(sheets1[i], sheets2[i]):
+                result.append(f"Excel1: {sheets1[i].sheet_name} | Excel2: {sheets2.sheet_name}")
+            result.append(sheets1[i].sheet_name)
+    else:
+        # TODO! 🤮 clean up
+        (smaller, smallerName), (bigger, biggerName) = get_smaller_sheet_amount(sheets1, sheets2)
+        for i in range(0, len(smaller)):
+            if not compare_sheet_name(sheets1[i], sheets2[i]):
+                result.append(f"Excel1: {sheets1[i].sheet_name} | Excel2: {sheets2.sheet_name}")
+            result.append(sheets1[i].sheet_name)
+        for i in range(len(smaller), len(bigger)):
+            if smallerName == "Excel1":
+                result.append(f"Excel1: {EMOJIS.get('ERROR')} | Excel2: {sheets2[i].sheet_name}")
+            if smallerName == "Excel2":
+                result.append(f"Excel1: {sheets1[i].sheet_name} | Excel2: {EMOJIS.get('ERROR')}")
+
+    return result
+
+
+def compare_sheet_dimensions(excel1: Excel, excel2: Excel):
+    # TODO! COMPARE SHEET DIMENSIONS ROWS AND COLUMNS FUTURE MAREK
+    sheetCount, same_amount = get_sheet_count(excel1, excel2)
+    sheets1 = excel1.sheets
+    sheets2 = excel2.sheets
+    result = []
+    if same_amount:
+        for i in range(0, sheetCount):
+            if compare_column_count(sheets1[i], sheets2[i]):
+                continue
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import logging
 
 from models.ExcelFile import Excel
 from comparator.SheetComparator import compare_all_sheet_properties, compare_sheet_name
+from comparator.ExcelComparator import compare_sheet_names_lists
 from constants.emoji import EMOJIS
 
 log = logging.getLogger(__name__)
@@ -19,28 +20,8 @@ def main():
 
     log.info("Getting the sheet count")
 
-    for i in range(0, sheetCount):
-        sheet1 = excel.sheets[i]
-        sheet2 = excel2.sheets[i]
-        log.info("Comparing the sheet values")
-
-        if compare_all_sheet_properties(sheet1, sheet2):
-            log.info("All sheet properties are the same")
-            log.info(f"{sheet1.sheet_name} - {EMOJIS.get('CHECKMARK')}")
-            log.info(f"{sheet2.sheet_name} - {EMOJIS.get('CHECKMARK')}")
-            continue
-
-        log.info("Some properties are not the same")
-
-        if not compare_sheet_name(sheet1, sheet2):
-            log.info("Sheet names are not the same")
-            log.info(f"{sheet1.sheet_name} - {EMOJIS.get('ERROR')}")
-            log.info(f"{sheet2.sheet_name} - {EMOJIS.get('ERROR')}")
-            continue
-        log.info("Sheet names are the same but some other property differs")
-
-        log.info(f"{sheet1.sheet_name} - {EMOJIS.get('WARNING')}")
-        log.info(f"{sheet2.sheet_name} - {EMOJIS.get('WARNING')}")
+    compared_sheets = compare_sheet_names_lists(excel, excel2)
+    print(compared_sheets)
 
 
 if __name__ == "__main__":
